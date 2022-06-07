0d1e6367ff247940a6c78aa6cee221856ed85649
facundoreyes <facundotomasreyes@gmail.com>
2022-06-06 21:54:44 -0300

    Updated files with errors.

--- simple/e2.sh
+++ simple/e2.sh
@@ -2,6 +2,6 @@
 
 #$1 path
 
-N_FILES=`ls $1 | grep -E "^[a-z]{2}.*\.(sh|txt)" | wc -l`
+N_FILES=`ls $1 | grep -E "^[a-z]{2}.*\.(sh|txt)$" | wc -l`
 
 echo "Hay $N_FILES archivos terminados en .sh o .txt que empiecen con 2 letras minúsculas"
--- simple/i3.sh
+++ simple/i3.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-CONTADOR=0
+CONTADOR_PALABRAS=$#
+CONTADOR_PALABRAS_TRES_LETRAS=0
 
-for PALABRA in $@
-do
-	CONTADOR=$(($CONTADOR + ${PALABRA} -gt 3 ? 1 : 0 ))
-done
-
-echo  $CONTADOR
+if [ $# -eq 0 ]
+then
+	echo "No hay argumentos"
+	# exit 1
+else
+	for PALABRA in $@
+	do
+		[ ${#PALABRA} -ge 3 ] && let CONTADOR_PALABRAS_TRES_LETRAS++
+	done
+	echo "Cantidad de palabras: $CONTADOR_PALABRAS"
+	echo "Cantidad de palabras con al menos 3 letras: $CONTADOR_PALABRAS_TRES_LETRAS"
+	#exit 0
+fi
